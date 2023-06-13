ebb5abcf95ea3396d00cffc68570ccd62e21ac5b
Louis301 <juri.orlik@yandex.ru>
2023-06-13 23:19:04 +0300

    Basic struct of Fool Game was initialized. Start of the project

---
+++ The_Fool_Game/Main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    const int n = 8, m = 5;
+
+    int matrix[n][m];
+    int i, j;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            matrix[i][j] = 0;
+
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            matrix[i][j] = 0;
+
+            std::cout.width(4);
+            std::cout << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+};
